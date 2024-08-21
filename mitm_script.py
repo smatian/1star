13677114294832ec28d3a1a7fa035b7fb5b3026f
BuildTools <unconfigured@null.spigotmc.org>
2024-08-20 22:00:27 -0700

    debugging

--- mitm_script.py
+++ mitm_script.py
@@ -2,6 +2,7 @@ from mitmproxy import http
 import json
 
 def response(flow: http.HTTPFlow) -> None:
+    print(flow.request.pretty_url)
     url = "https://oneroster.lvusd.org/oneroster/mobileapi/v1//student/20112478/classsummary"
     if flow.request.pretty_url == url:
         data = json.loads(flow.response.text)
