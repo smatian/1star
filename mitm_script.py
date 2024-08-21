d025e7656b174487cf9425b4895b501fe67046bb
BuildTools <unconfigured@null.spigotmc.org>
2024-08-20 21:45:27 -0700

    use different script for this

--- mitm_script.py
+++ mitm_script.py
@@ -2,7 +2,18 @@ from mitmproxy import http
 import json
 
 def response(flow: http.HTTPFlow) -> None:
-    if "application/json" in flow.response.headers.get("content-type", ""):
-        data = json.loads(flow.response.content)
-        data["test"] = "Hello, World"
+    url = "https://oneroster.lvusd.org/oneroster/mobileapi/v1//student/20112478/classsummary"
+    if flow.request.pretty_url == url:
+        data = json.loads(flow.response.text)
+        if 'ClassSummary' in data[0]:
+            for j in range(len(data[0]['ClassSummary'])):
+               
+
+                if 'CurrentMark' in data[0]['ClassSummary'][j]:
+                    data[0]['ClassSummary'][j]['CurrentMark'] = "A+"
+                
+                if 'Percent' in data[0]['ClassSummary'][j]:
+                    data[0]['ClassSummary'][j]['Percent'] = 100.0
+                    
+                    
         flow.response.text = json.dumps(data)
