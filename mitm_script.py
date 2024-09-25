e726db4e812a6e73f1ba86458c526b31a01449c2
Sina Matian <89218912+smatian@users.noreply.github.com>
2024-09-25 15:58:33 -0700

    use github

--- mitm_script.py
+++ mitm_script.py
@@ -1,5 +1,6 @@
 from mitmproxy import http
 import json
+import requests
 
 badge_data = [
     {
@@ -7,32 +8,32 @@ badge_data = [
         "Name": "OFF CAMPUS 1A",
         "Type": "Activity",
     },
-   
-        {
-            "Type": "Activity",
-            "Name": "ASB CARD",
-            "ImageFileName": "cc3f97dd82b04109a9cd608334508bb3.png",
-        },
-    
+    {
+        "Type": "Activity",
+        "Name": "ASB CARD",
+        "ImageFileName": "cc3f97dd82b04109a9cd608334508bb3.png",
+    },
 ]
 
 def response(flow: http.HTTPFlow) -> None:
     # Define the URLs you want to intercept
     
-
-
     if "https://api.5starstudents.com/person/identity" in flow.request.pretty_url:
         data = json.loads(flow.response.content)
-        print(data["Number"])
-        
-        # check if data["number"] is in a line on allowed.txt
-        with open("allowed.txt", "r") as f:
-            allowed_numbers = f.read().splitlines()
-            if data["Number"] in allowed_numbers:
-                 data["Badges"] = badge_data
-                 flow.response.content = json.dumps(data).encode()
-            
-            else :
-                data["Badges"] = []
-        
-       
+        print(data)
+
+        # Load the allowed numbers from the GitHub URL
+        url = "https://raw.githubusercontent.com/smatian/1star/main/allowed.txt"
+        response = requests.get(url)
+        if response.status_code == 200:
+            allowed_numbers = response.text.splitlines()
+        else:
+            allowed_numbers = []
+
+        # Check if the number is in the allowed list
+        if data["Number"] in allowed_numbers:
+            data["Badges"] = badge_data
+        else:
+            data["Badges"] = []
+
+        flow.response.content = json.dumps(data).encode()
